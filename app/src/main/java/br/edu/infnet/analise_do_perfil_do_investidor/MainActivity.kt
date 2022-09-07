4a1a7200c71ae61ef7fe6bcc07c16c1099b4cf9a
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-09-07 15:28:50 -0300

    Implementando mundança de tela ao clicar sobre button

--- app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.edu.infnet.analise_do_perfil_do_investidor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.infnet.analise_do_perfil_do_investidor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAvancarTela.setOnClickListener {
+            val nome = binding.inputNome.text.toString()
+            val intent = Intent(this, QuestionsActivity::class.java)
+            intent.putExtra(NOME, nome)
+            startActivity(intent)
+        }
+    }
+    companion object {
+        val NOME = "NOME"
     }
 }
