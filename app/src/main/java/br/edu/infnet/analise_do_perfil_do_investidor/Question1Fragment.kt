731640ee6a7faf667c7a6be741307316e9961143
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-09-08 01:09:09 -0300

    Criando navigation entre Question1Activity e Question2Activity

--- app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/Question1Fragment.kt
+++ app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/Question1Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import br.edu.infnet.analise_do_perfil_do_investidor.databinding.FragmentQuestion1Binding
 
 
@@ -16,9 +17,16 @@ class Question1Fragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentQuestion1Binding.inflate(inflater, container, false)
         val view  = binding.root
+
+        binding.btnAvancarQ1.setOnClickListener {
+
+            view.findNavController().navigate(R.id.action_question1Fragment_to_question2Fragment)
+
+        }
+
         return view
     }
 
