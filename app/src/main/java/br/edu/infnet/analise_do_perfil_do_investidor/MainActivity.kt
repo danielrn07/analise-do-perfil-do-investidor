b9df24df777385dbcaf879ec6805dbfb41a3929f
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-09-08 05:58:45 -0300

    Criando aba contendo informações sobre o projeto

--- app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/analise_do_perfil_do_investidor/MainActivity.kt
@@ -3,7 +3,15 @@ package br.edu.infnet.analise_do_perfil_do_investidor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import br.edu.infnet.analise_do_perfil_do_investidor.databinding.ActivityMainBinding
+import br.edu.infnet.analise_do_perfil_do_investidor.databinding.CustomDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,8 +29,25 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(NOME, nome)
             startActivity(intent)
         }
+
+        findViewById<Button>(R.id.btn_sobre).setOnClickListener {
+            showCloseAlert()
+        }
+
+
     }
     companion object {
         val NOME = "NOME"
     }
+
+    private fun showCloseAlert() {
+        val alert: AlertDialog.Builder = AlertDialog.Builder(this, R.style.ThemeCustomDialog)
+        val dialogBinding: CustomDialogBinding = CustomDialogBinding
+            .inflate(LayoutInflater.from(this))
+
+        alert.setView(dialogBinding.root)
+        alert.show()
+    }
+
+
 }
